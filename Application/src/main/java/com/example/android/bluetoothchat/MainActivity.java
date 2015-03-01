f2923e00017f52b1028df97d0d5ede70cf8f5266
Nick <xdedalusx@gmail.com>
2015-03-01 18:09:43 -0500

    Commit from another PC after checking out from GitHub in Android Studio

--- Application/src/main/java/com/example/android/bluetoothchat/MainActivity.java
+++ Application/src/main/java/com/example/android/bluetoothchat/MainActivity.java
@@ -37,6 +37,7 @@ import com.example.android.common.logger.MessageOnlyLogFilter;
  */
 
 //to be modified for Senior Project
+//another modification from another PC after checking out from GitHub in Android Studio
 public class MainActivity extends SampleActivityBase {
 
     public static final String TAG = "MainActivity";
