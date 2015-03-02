52b5c052d7ea8acba292ddefacb82c330946a40b
Asvin Tom Joseph <ajosep12@nyit.edu>
2015-03-01 20:51:57 -0500

    Commit from another PC after checking out from GitHub in Android Studio

--- Application/src/main/java/com/example/android/bluetoothchat/DeviceListActivity.java
+++ Application/src/main/java/com/example/android/bluetoothchat/DeviceListActivity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+// I am changing something
 package com.example.android.bluetoothchat;
 
 import android.app.Activity;
