907a9f40dc35c64a413a2cd0e26730420b06c1de
Asvin Tom Joseph <ajosep12@nyit.edu>
2015-03-09 11:09:04 -0400

    Commit from another PC after checking out from GitHub in Android Studio

---
+++ Application/src/main/java/com/example/android/bluetoothchat/CustomizeAppearancePage.java
@@ -0,0 +1,30 @@
+package com.example.android.bluetoothchat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class CustomizeAppearancePage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
