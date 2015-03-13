2d65e3706bdd48512ee7f597f947bea90fc488b3
Asvin Tom Joseph <ajosep12@nyit.edu>
2015-03-13 09:53:02 -0400

    Asvin Commit 3/13/2015

--- Application/src/main/java/com/example/android/bluetoothchat/CustomizeAppearancePage.java
+++ Application/src/main/java/com/example/android/bluetoothchat/CustomizeAppearancePage.java
@@ -4,14 +4,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class CustomizeAppearancePage extends Activity {
+public class CustomizeAppearancePage extends Activity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_customize_appearance_page);
         Intent intent = getIntent();
+
+        findViewById(R.id.button1).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+        findViewById(R.id.button3).setOnClickListener(this);
     }
 
     @Override
@@ -27,5 +34,23 @@ public class CustomizeAppearancePage extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v){
+
+        // TODO Auto-generated method stub
+
+        switch(v.getId()){
+            case R.id.button1:
+                Utils.changeToTheme(this, Utils.THEME_DEFAULT);
+                break;
+            case R.id.button2:
+                Utils.changeToTheme(this, Utils.THEME_WHITE);
+                break;
+            case R.id.button3:
+                Utils.changeToTheme(this, Utils.THEME_BLUE);
+                break;
+        }
     }
 }
