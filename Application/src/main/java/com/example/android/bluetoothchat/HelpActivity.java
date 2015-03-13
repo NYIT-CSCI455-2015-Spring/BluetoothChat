6cee8d302ebbadd3273743e293dced2f0994afe3
Asvin Tom Joseph <ajosep12@nyit.edu>
2015-03-13 10:17:22 -0400

    Asvin Commit 3/13/2015 latest

--- Application/src/main/java/com/example/android/bluetoothchat/HelpActivity.java
+++ Application/src/main/java/com/example/android/bluetoothchat/HelpActivity.java
@@ -1,18 +1,15 @@
 package com.example.android.bluetoothchat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.view.View.OnClickListener;
 import android.view.View;
-import android.content.Intent;
-import android.widget.EditText;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 
 public class HelpActivity extends Activity {
--- Application/src/main/java/com/example/android/bluetoothchat/ThemeActivity.java
+++ Application/src/main/java/com/example/android/bluetoothchat/ThemeActivity.java
@@ -4,14 +4,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class ThemeActivity extends Activity {
+public class ThemeActivity extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_customize_appearance_page);
         Intent intent = getIntent();
+
+        findViewById(R.id.button1).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+        findViewById(R.id.button3).setOnClickListener(this);
     }
 
     @Override
@@ -27,5 +34,22 @@ public class ThemeActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v){
+
+        switch(v.getId()){
+
+            case R.id.button1:
+                Utils.changeToTheme(this, Utils.THEME_DEFAULT);
+                break;
+            case R.id.button2:
+                Utils.changeToTheme(this, Utils.THEME_WHITE);
+                break;
+            case R.id.button3:
+                Utils.changeToTheme(this, Utils.THEME_BLUE);
+                break;
+        }
     }
 }
