1a61bf31c63b0480cc5fc8a0c6b37807d8b44894
Asvin Tom Joseph <ajosep12@nyit.edu>
2015-04-03 11:32:14 -0400

    Asvin Commit April 3, 2015 Database

---
+++ Application/src/main/java/com/example/android/bluetoothchat/ContactInfoActivity.java
@@ -0,0 +1,73 @@
+package com.example.android.bluetoothchat;
+
+/**
+ * Created by Asvin on 4/2/15.
+ */
+public class ContactInfoActivity {
+
+    // private variables
+    public int _id;
+    public String _name;
+    public String _phone_number;
+    public String _email;
+
+    public ContactInfoActivity() {
+    }
+
+    // constructor
+    public ContactInfoActivity(int id, String name, String _phone_number, String _email) {
+        this._id = id;
+        this._name = name;
+        this._phone_number = _phone_number;
+        this._email = _email;
+
+    }
+
+    // constructor
+    public ContactInfoActivity(String name, String _phone_number, String _email) {
+        this._name = name;
+        this._phone_number = _phone_number;
+        this._email = _email;
+    }
+
+    // getting ID
+    public int getID() {
+        return this._id;
+    }
+
+    // setting id
+    public void setID(int id) {
+        this._id = id;
+    }
+
+    // getting name
+    public String getName() {
+        return this._name;
+    }
+
+    // setting name
+    public void setName(String name) {
+        this._name = name;
+    }
+
+    // getting phone number
+    public String getPhoneNumber() {
+        return this._phone_number;
+    }
+
+    // setting phone number
+    public void setPhoneNumber(String phone_number) {
+        this._phone_number = phone_number;
+    }
+
+    // getting email
+    public String getEmail() {
+        return this._email;
+    }
+
+    // setting email
+    public void setEmail(String email) {
+        this._email = email;
+    }
+
+}
